35854e463db87ea7fcb2630c6c56bd49cf58474a
Raj <97rajmath@gmail.com>
2020-09-06 11:14:47 -0400

    User Auth API

---
+++ blogging-app-backend/Routes/Users.js
@@ -0,0 +1,8 @@
+const express=require('express');
+const app=express();
+const router=express.Router();
+
+router.get('./signin',(req,res)=>{})
+router.post('./register',(req,res)=>{})
+
+module.exports=router;
