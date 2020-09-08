68bcd1817687d162f9e2abde655c4dbd226916a4
Raj <97rajmath@gmail.com>
2020-09-08 12:21:40 -0400

    State managment

--- blogging-app-backend/Routes/Blogs.js
+++ blogging-app-backend/Routes/Blogs.js
@@ -6,36 +6,7 @@ const mongoose=require('mongoose');
 let Post=require('../Models/post');
 const { response } = require('express');
 
-const Blogs=[
-    {
-        id:1,
-        title:"post one",
-        body: "post one body",
-        author:"Max",
-        date:new Date()
-    },
-    {
-        id:2,
-        title:"post 2",
-        body: "post 2 body",
-        author:"Sam",
-        date:new Date()
-    },
-    {
-        id:3,
-        title:"post 3",
-        body: "post 3 body",
-        author:"Jason",
-        date:new Date()
-    },
-    {
-        id:4,
-        title:"post 4",
-        body: "post 4 body",
-        author:"Peter",
-        date:new Date()
-    }
-];
+
 //Show all posts
 router.get('/',(req,res)=>{
     Post.find({},(error,posts)=>{
@@ -50,15 +21,12 @@ router.get('/',(req,res)=>{
 
 //Show post by id
 router.get('/:id',(req,res)=>{
-    Post.findById(req.params.id,(error,post)=>{
-        if(error) throw new Error(error);
-        console.log(post);
-        res.status(200).send(`
-            <h1>${post.title}</h1>
-            <p>${post.body}</p>
-        `);
-        
-    });
+    let query={_id : req.params.id}
+    Post.findOne(query,(err,response)=>{
+        if(!err){
+            res.status(200).json(response);
+        }
+    })
 });
 
 //Add Post
